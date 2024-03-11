261dc9e37cf1cd8c868f7c0e87c9fc616e76975b
Dan Wilkinson <jl22397@bristol.ac.uk>
2024-03-11 12:22:18 +0000

    updated and finalised first draft of DataController for testing.

--- DataController/DataController.vhd
+++ DataController/DataController.vhd
@@ -7,6 +7,7 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 USE work.ALL;
+USE work.common_pack.all;
 
 entity dataConsume is
 port (
@@ -31,24 +32,51 @@ ARCHITECTURE behavioural OF dataConsume IS
 
 -- State Declaration
 -- ALL STATE TYPES
-	TYPE state_type IS (INIT, ); 
+	TYPE state_type IS (INIT, DONE, FIRST_THREE_ctrlSet, FIRST_THREE_ctrlWait,
+        FIRST_THREE_regOn, FIRST_THREE_decision, MAIN_LOOP_ctrlSet, MAIN_LOOP_ctrlWait,
+        MAIN_LOOP_regOn, MAIN_LOOP_decision, LOOP_END_regOn, LOOP_END_decision
+      );
+ 
 -- Signal Declaration
 	signal ctrlIn_delayed, ctrlIn_detected: std_logic;
 	SIGNAL start_reg: std_logic;
 
-	
 	SIGNAL reg6, reg5, reg4, reg3, reg2, reg1, reg0: unsigned(7 DOWNTO 0);
-	
-        SIGNAL data0, data1, data2, data3, data4, data5, data6: UNSIGNED(7 DOWNTO 0);
-	
+        --SIGNAL data0, data1, data2, data3, data4, data5, data6: UNSIGNED(7 DOWNTO 0);
+
 	SIGNAL numWords_add3: integer range 3 to 1002;
 	SIGNAL numWords_int: integer range 0 to 999;
 	SIGNAL counter: integer range 0 to 999;
-	SIGNAL maxValue: unsigned(7 DOWNTO 0);
+	--SIGNAL maxValue: unsigned(7 DOWNTO 0);
 BEGIN
 
 -------------------------------------------------------------------
-combi_out: 
+  combi_out: PROCESS(cur_state)
+  BEGIN
+	dataReady <= '0';
+	byte <= '00000000';
+	seqDone <= '0';
+	
+	IF cur_state = FIRST_THREE_regOn
+	OR cur_state = MAIN_LOOP_regOn THEN
+
+	  byte <= data;
+	  dataReady <= '1';
+
+	END IF;
+	IF cur_state = DONE THEN
+	  seqDone = '1';
+	END IF;
+  END PROCESS;
+
+--- there is a potential issue with this combi_out state stuff. If the max Value is the very final one in the sequence, it may not actually output this. However,
+-- I think this is unlikely because the last three digits are always 0. However, it is worth trying this to see.
+
+
+
+
+
+
 
 --fill in list
 
@@ -75,14 +103,14 @@ combi_out:
 
 
 
-  word_converter: PROCESS(numWords_bcd)
+  bcd_to_integer: PROCESS(numWords_bcd)
 	    -- variable of type integer with constrained range and initial value
 
   VARIABLE tmp:INTEGER RANGE 0 TO 999:=0;  -- store the sum
 
   BEGIN
   FOR i IN 0 TO 2 LOOP
-    tmp:=tmp+TO_INTEGER(numWords_bcd(i))*(10^(2-i);
+    tmp:=tmp+TO_INTEGER(numWords_bcd(i))*(10**(2-i));
   
   END LOOP;
 
@@ -93,6 +121,16 @@ combi_out:
 --make something that converts the integer back to a thing.
 
 
+  Integer_to_bcd: Process(maxIndex_int)
+	Variable tmp: integer range 0 to 99;
+  Begin
+	maxIndex(0) <= to_unsigned((maxIndex_int / 100), 4);
+-- can I convert this to vector in same line?
+--vhdl integers really round down?
+	tmp:= maxIndex_int rem 100;
+	MaxIndex(1) <= to_unsigned((tmp / 10), 4);
+	MaxIndex(2) <= to_unsigned((tmp mod 10), 4);
+  End process;
 
 
 
@@ -150,13 +188,13 @@ BEGIN
 	reg2 <= TO_UNSIGNED(0,8);
 	reg1 <= TO_UNSIGNED(0,8);
 	reg0 <= TO_UNSIGNED(0,8);
-	data0 <= TO_UNSIGNED(0,8);
-	data1 <= TO_UNSIGNED(0,8);
-	data2 <= TO_UNSIGNED(0,8);
-	data3 <= TO_UNSIGNED(0,8);
-	data4 <= TO_UNSIGNED(0,8);
-	data5 <= TO_UNSIGNED(0,8);
-	data6 <= TO_UNSIGNED(0,8);
+	--data0 <= TO_UNSIGNED(0,8);
+	--data1 <= TO_UNSIGNED(0,8);
+	--data2 <= TO_UNSIGNED(0,8);
+	--data3 <= TO_UNSIGNED(0,8);
+	--data4 <= TO_UNSIGNED(0,8);
+	--data5 <= TO_UNSIGNED(0,8);
+	--data6 <= TO_UNSIGNED(0,8);
 	maxValue <= TO_UNSIGNED(0,8);
 
 	counter = 0;
@@ -165,7 +203,7 @@ BEGIN
 
 --all registers to zero, then:
 	if start_reg = '1' then
-	  nextState <= REG6_A;
+	  nextState <= FIRST_THREE_ctrlSet;
 	else
 	  nextState <= INIT;
 	end if;
@@ -180,7 +218,7 @@ BEGIN
 -------- Register 6 States (NORMAL CONDITIONS)
       WHEN FIRST_THREE_ctrlSet =>
 	ctrlOut <= not ctrlOut;
-	nextState <= REG6_B;
+	nextState <= FIRST_THREE_ctrlWait;
 
       WHEN FIRST_THREE_ctrlWait =>
 	if ctrlIn_detected = '1' then 
@@ -192,8 +230,8 @@ BEGIN
 	reg6 <= UNSIGNED(data);
 	reg5 <= reg6;
 	reg4 <= reg5;
-	??--reg3 <= reg4;
-	counter <= counter +1;
+	--??--reg3 <= reg4;
+	counter <= counter + 1;
 	nextState <= FIRST_THREE_decision;
       
       WHEN FIRST_THREE_decision =>
@@ -235,7 +273,7 @@ BEGIN
 	reg1 <= reg2;
 	reg0 <= reg1;
 	counter <= counter +1;
-	if reg4 > maxValue then
+	if reg4 > dataResults(3) then
 	  dataResults(0) <= std_logic_vector(reg1);
 	  dataResults(1) <= std_logic_vector(reg2);
 	  dataResults(2) <= std_logic_vector(reg3);
@@ -243,8 +281,8 @@ BEGIN
 	  dataResults(4) <= std_logic_vector(reg5);
 	  dataResults(5) <= std_logic_vector(reg6);
 	  dataResults(6) <= data;
-	  maxValue <= std_logic_vector(reg4);
 	  maxIndex <= counter -3;
+
 ---- maybe add more things here?
 
 	end if;
@@ -279,7 +317,7 @@ BEGIN
 	reg1 <= reg2;
 	reg0 <= reg1;
 	counter <= counter +1;
-	if reg4 > maxValue then
+	if reg4 > dataResults(3) then
 	  dataResults(0) <= std_logic_vector(reg1);
 	  dataResults(1) <= std_logic_vector(reg2);
 	  dataResults(2) <= std_logic_vector(reg3);
@@ -287,10 +325,10 @@ BEGIN
 	  dataResults(4) <= std_logic_vector(reg5);
 	  dataResults(5) <= std_logic_vector(reg6);
 	  dataResults(6) <= "00000000";
-	  maxValue <= std_logic_vector(reg4);
-	  maxIndex <= counter -3;
+	  
+	  maxIndex <= counter - 3;
 	end if;
-	
+
 	nextState <= LOOP_END_decision;
 
 
@@ -300,7 +338,8 @@ BEGIN
 	else
 	  nextState <= DONE;
 
-END PROCESS; 
+END PROCESS;
+
 END behavioural;
 
 
